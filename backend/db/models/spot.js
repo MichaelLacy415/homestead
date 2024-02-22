b7672fede961cc467cf42bf1e08d7a7a5e995319
Michael <michael_lacy95@icloud.com>
2024-02-22 16:33:38 -0600

    spots data

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -4,45 +4,55 @@ const { Model, Validator } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
     static associate(models) {
-      // define association here
+      Spot.belongsTo(models.User, {foreignKey: 'ownerId'});
     }
   };
-  
+
   Spot.init({
     ownerId: {
       type: DataTypes.INTEGER,
+      allowNull: false,
     },
     address: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     city: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     state: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     country: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     lat: {
       type: DataTypes.FLOAT,
+      allowNull: false,
     },
     lng: {
       type: DataTypes.FLOAT,
+      allowNull: false,
     },
     name: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     description: {
       type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
       type: DataTypes.INTEGER,
+      allowNull: false,
 
     }
   }, {
     sequelize,
-    modelName: 'spot',
+    modelName: 'Spot',
   });
   return Spot;
 };
