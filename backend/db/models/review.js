5b1183f80c6f8197d7da9da01f90835ae2e3515b
Michael <michael_lacy95@icloud.com>
2024-02-22 20:47:31 -0600

    reviews data

---
+++ backend/db/models/review.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    static associate(models) {
+      Review.belongsTo(models.User, {foreignKey: 'userId'});
+      Review.belongsTo(models.Spot, {foreignKey: 'spotId'})
+    }
+  };
+  
+  Review.init({
+    review: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
---
+++ backend/db/seeders/20240223013743-reviews.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { Review } = require('../models');
+
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await Review.bulkCreate([
+      {
+        review: "this place is amazing",
+        stars: 4,
+        userId: 1,
+        spotId: 1
+      },
+      {
+        review: "Best place ive ever been too",
+        stars: 5,
+        userId: 2,
+        spotId: 2
+      },
+      {
+        review: "CHIEF KINGDOM!!!!!!",
+        stars: 4,
+        userId: 3,
+        spotId: 3
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Reviews';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+    }, {});
+  }
+};
